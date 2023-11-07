d4b27523f9ab8f4a55a452be98c4a1d08cab001e
Victor <btf.victorsf0206@gmail.com>
2023-11-07 18:53:54 -0300

    Implementada a classe "Colaborador"

---
+++ ModalGR_Emprestimo/Emprestimo/Colaborador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModalGR_Emprestimo.Emprestimo
+{
+    internal class Colaborador
+    {
+        public string Nome { get; set; }
+
+        public DateTime DataDeAdmissão { get; set; }
+
+        public double Salario { get; set; }
+
+        public double ValorDeEmprestimo { get; set; }
+
+
+    }
+}
