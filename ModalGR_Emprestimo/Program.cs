6276f0b612206ac31f39528c100a78be78242ac0
Victor <btf.victorsf0206@gmail.com>
2023-11-07 18:54:38 -0300

    implementada o fluxo principal

--- ModalGR_Emprestimo/Program.cs
+++ ModalGR_Emprestimo/Program.cs
@@ -1,6 +1,23 @@
-﻿
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using ModalGR_Emprestimo.Emprestimo;
 
 var emprestimo = new SimuladorDeEmprestimo();
 
-emprestimo.Exibe
+try
+{
+    emprestimo.ExibeTitulo();
+    emprestimo.ColetaDadosCadastrais();
+
+    bool tempoAdmissionalValido = emprestimo.ValidaTempoAdmissional();
+    if (tempoAdmissionalValido == false) return;
+
+    emprestimo.ValidaValorPretendido();
+
+    var grupoDenotas = emprestimo.DefineGrupoDeNotas();
+    emprestimo.ExibeNotasSelecionadas(grupoDenotas);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Erro: " + ex.Message);
+}
